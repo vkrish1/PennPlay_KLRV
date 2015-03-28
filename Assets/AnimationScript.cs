73796f80e95607ade6b706c403daa8e42d405529
kbreitton <kbreitton@gmail.com>
2015-03-28 19:07:54 -0400

    from klyde

---
+++ Assets/AnimationScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationScript : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0) {
+			anim.SetInteger ("AnimationState", 1);
+			//Debug.Log ("Walking");
+		} else {
+			anim.SetInteger ("AnimationState", 0);
+			//Debug.Log ("Still");
+		}
+	}
+}
