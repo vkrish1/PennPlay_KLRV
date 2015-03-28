ce6eb7c8f196423b5a665a7bdfb61624a878ea3d
vkrish1 <veena.t.krish@gmail.com>
2015-03-28 14:02:47 -0400

    Updated SwitchColors.cs

--- Assets/SwitchColors.cs
+++ Assets/SwitchColors.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class SwitchWorld : MonoBehaviour {
 
+	bool startSwitchIntoColor = true;
+
 	// Use this for initialization
 	void Start () {
-	
+		startSwitchIntoColor = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		//generate a random color
+		Color myColor = new Color(Random.Range(0.0,1.0),Random.Range(0.0,1.0),Random.Range(0.0,1.0));
+
+		//find the person we're going to change
+		Transform person = this.transform;
+
+		//change the color of that person to myColor
+		if(startSwitchIntoColor == true){			
+			person.renderer.material.color = myColor;
+		}
 	
 	}
 }
