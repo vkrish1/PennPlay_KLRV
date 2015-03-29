79e41c216f457ae6ed36e8ed86dc09bafbdb013e
kbreitton <kbreitton@gmail.com>
2015-03-28 21:35:41 -0400

    everything

---
+++ Assets/AudioControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioControl : MonoBehaviour {
+
+	AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+		audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/SwitchColors.cs
+++ Assets/SwitchColors.cs
@@ -1,28 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SwitchWorld : MonoBehaviour {
+public class SwitchColors : MonoBehaviour {
 
-	bool startSwitchIntoColor = true;
+	Renderer rend;
+	public Color white = Color.white;
+
+	public Color orig_color;
+
+	public bool isColor;
 
 	// Use this for initialization
 	void Start () {
-		startSwitchIntoColor = true;
+		rend = GetComponent<Renderer> ();
+		orig_color = rend.material.color;
+		isColor = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		//generate a random color
-		Color myColor = new Color(Random.Range(0.0,1.0),Random.Range(0.0,1.0),Random.Range(0.0,1.0));
-
-		//find the person we're going to change
-		Transform person = this.transform;
-
-		//change the color of that person to myColor
-		if(startSwitchIntoColor == true){			
-			person.renderer.material.color = myColor;
-		}
+		switch_colors (isColor);
 	
+	}
+
+	void switch_colors (bool is_color) {
+		if (is_color) {
+			if (Input.GetKeyDown (KeyCode.F)) {
+				rend.material.SetColor ("_Color", white);
+				rend.material.SetColor ("_Emission", white);
+				isColor = (!is_color);
+			}
+		}
+		else {
+			if (Input.GetKeyDown (KeyCode.F)) {
+				rend.material.SetColor ("_Color", orig_color);
+				rend.material.SetColor ("_Emission", orig_color);
+				isColor = (!is_color);
+			}
+		}
 	}
 }
